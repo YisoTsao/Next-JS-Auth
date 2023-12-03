a7cef51846cd23492fe15f0756208ec8242b1481
garytsao525 <garytsao@kingsinfo.com.tw>
2023-12-03 22:50:02 +0800

    add basic Credentials auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import LineProvider from "next-auth/providers/line";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   providers: [
@@ -16,6 +17,34 @@ const handler = NextAuth({
     LineProvider({
       clientId: process.env.LINE_CLIENT_ID,
       clientSecret: process.env.LINE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign-in form (e.g., 'Sign in with...')
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        // Add custom authentication logic here
+        // You can use the credentials.username and credentials.password for validation
+
+        console.log(credentials?.password);
+        // const res = await fetch("/your/endpoint", {
+        //   method: "POST",
+        //   body: JSON.stringify(credentials),
+        //   headers: { "Content-Type": "application/json" },
+        // });
+        // const user = await res.json();
+
+        if (credentials) {
+          // If the credentials are valid, return the user object
+          return Promise.resolve(credentials);
+        } else {
+          // If the credentials are invalid, return null
+          return Promise.resolve(null);
+        }
+      },
     }),
   ],
   callbacks: {
--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -1,10 +1,46 @@
 "use client";
 
+import { useState } from "react";
 import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
+  const [credentials, setCredentials] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await signIn("credentials", {
+      ...credentials,
+      redirect: false,
+    });
+    console.log(result);
+  };
+
   return (
     <div className="flex gap-4">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            onChange={(e) =>
+              setCredentials({ ...credentials, username: e.target.value })
+            }
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            onChange={(e) =>
+              setCredentials({ ...credentials, password: e.target.value })
+            }
+          />
+        </label>
+        <br />
+        <button type="submit">Sign In</button>
+      </form>
+
       <button
         onClick={() => signIn("line")}
         className="mt-4 bg-slate-800 text-white px-6 py-3 rounded-lg"
@@ -24,7 +60,6 @@ export default function LoginButton() {
       >
         Sign in with google
       </button>
-
     </div>
   );
 }
